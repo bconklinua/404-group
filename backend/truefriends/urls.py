3222f0328c0d426abfbe8fb1bc41ab5445d3d290
AGLandberg <landberg@ualberta.ca>
2022-10-24 19:24:35 -0600

    Add Post model and API functionality for POST

--- backend/truefriends/urls.py
+++ backend/truefriends/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from apps.Post import views
 
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostView, 'posts')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('service/authors/authorID/', include(router.urls)),
 ]
