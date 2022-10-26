96f20ba1f17d6a77c6f2c3c389de6de1361c7ba8
Brad Conklin <77289199+bconklinua@users.noreply.github.com>
2022-10-26 15:57:20 -0600

    login api endoing /api/auth/login/

--- backend/apps/User/serializers.py
+++ backend/apps/User/serializers.py
@@ -1,7 +1,55 @@
+from abc import ABC
+
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
+
+
+class LoginSerializer(serializers.Serializer):
+    """
+    This serializer defines two fields for authentication:
+      * email
+      * password.
+    It will try to authenticate the user with when validated.
+    """
+    email = serializers.CharField(
+        label="Email",
+        write_only=True
+    )
+    password = serializers.CharField(
+        label="Password",
+        # This will be used when the DRF browsable API is enabled
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+
+    def validate(self, attrs):
+        # Take username and password from request
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        print("email: " + email)
+        print("password: " + password)
+
+        if email and password:
+            # Try to authenticate the user using Django auth framework.
+            user = authenticate(request=self.context.get('request'),
+                                username=email, password=password)
+            if not user:
+                # If we don't have a regular user, raise a ValidationError
+                msg = 'Access denied: wrong username or password.'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Both "username" and "password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+        # We have a valid user, put it in the serializer's validated_data.
+        # It will be used in the view.
+        attrs['user'] = user
+        return attrs
--- backend/apps/User/views.py
+++ backend/apps/User/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets, response, status
+from rest_framework import viewsets, response, status, permissions
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from . import serializers
 from .serializers import AuthorSerializer
 from .models import Author
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
 
 
 class AuthorView(viewsets.ModelViewSet):
@@ -22,3 +25,16 @@ class RegisterAPIView(GenericAPIView):
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    # This view should be accessible also for unauthenticated users.
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = serializers.LoginSerializer(data=self.request.data,
+                                                 context={'request': self.request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
