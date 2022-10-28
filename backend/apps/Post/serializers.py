ad3416ac975edf2c24c790e28fcfb2c1eff979b1
AGLandberg <landberg@ualberta.ca>
2022-10-28 02:45:40 -0600

    Fix creating and getting Posts to work with authenticated user. Make it so APIs can be tested with session based instead of JWT authentication

--- backend/apps/Post/serializers.py
+++ backend/apps/Post/serializers.py
@@ -1,3 +1,4 @@
+from typing import OrderedDict
 from rest_framework import serializers
 from .models import Category, Post, Like
 
@@ -9,13 +10,13 @@ class CategorySerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('type', 'title', 'id', 'source', 'origin', 'description', 
-    'contentType', 'content', 'author', 'categories', 'comments', 'count', 'published',
+        fields = ('type','author', 'title', 'id', 'source', 'origin', 'description', 
+    'contentType', 'content', 'categories', 'count', 'published',
     'visibility', 'unlisted')
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        rep["categories"] = [cat['name'] for cat in CategorySerializer(instance.categories.all(), many=True).data]
-        rep["count"] = Like.objects.filter(post_id = instance.id).count()
+        if not isinstance(instance, OrderedDict):
+            rep["categories"] = [cat['name'] for cat in CategorySerializer(instance.categories.all(), many=True).data]
         return rep
 
--- backend/apps/Post/views.py
+++ backend/apps/Post/views.py
@@ -1,16 +1,31 @@
+from venv import create
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
 from apps.User.models import Author
+from rest_framework import viewsets, status, permissions
 
 # Create your views here.
 
 class PostView(viewsets.ModelViewSet):
     serializer_class = PostSerializer
-
     def get_queryset(self):
         user_id = self.kwargs['author_id']
         if user_id:
             return Post.objects.filter(author_id=user_id)
         return super().get_queryset()
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = PostSerializer(instance=instance)
+        serializer.data['count'] = instance.count
+        return Response(serializer.data)
