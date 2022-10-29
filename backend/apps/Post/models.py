a7161318165d33ffefa1a689872e5488672fdf9a
AGLandberg <landberg@ualberta.ca>
2022-10-29 16:27:15 -0600

    Posts are sent to appropraite inboxes.

--- backend/apps/Post/models.py
+++ backend/apps/Post/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from ..Follow.models import Follow
 from ..User.models import Author
 from ..Like.models import Like
 from ..Inbox.models import Inbox
@@ -53,6 +54,22 @@ class Post(models.Model):
     def count(self):
         return Like.objects.filter(post_id = self.id).count()
 
+    def publish_post(self):
+        #Get list of people who are following the author of this post
+        follower_list = [f.follower for f in Follow.objects.filter(followee=self.author)]
+        following_list = [f.followee for f in Follow.objects.filter(follower=self.author)]
+        
+        for follower in follower_list:
+            try:
+                inbox = Inbox.objects.get(author=follower)
+            except ObjectDoesNotExist:
+                inbox = Inbox.objects.create(author=follower)
+            if self not in inbox.posts.all():
+                if self.visibility == "FRIENDS" and (follower not in following_list):
+                    continue
+                inbox.posts.add(self)
+        return
+
 @receiver(post_save, sender=Post)
 def add_post(instance, created, **kwargs):
     try:
@@ -64,3 +81,9 @@ def add_post(instance, created, **kwargs):
         user_inbox.posts.create(instance)
     elif instance not in user_inbox.posts.all():
         user_inbox.posts.add(instance)
+    #If the post is not marked as "unlisted", then publish the post
+    if not instance.unlisted:
+        instance.publish_post()
+    
+
+
