efaeef80ddfd67322588aba58a859eea1eaa450b
Saurab Harish Prabhakar <saurab@ualberta.ca>
2022-10-31 17:50:46 -0600

    View Posts and 404 not found

--- frontend/src/components/PostView/PostView.js
+++ frontend/src/components/PostView/PostView.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { NavLink, resolvePath, useNavigate, useParams } from "react-router-dom";
 import { test, getInbox, getPost } from '../../api/Post';
-import { getToken, validJWT } from '../../api/User';
+import { getToken, refreshToken, validJWT } from '../../api/User';
 import './PostView.css'
 import no_img from './no-image-icon-4.png';
 import { getPostByID } from '../../api/Post';
@@ -20,25 +20,88 @@ export default function PostView() {
             <h1> post_id: { post_id } </h1>
         );
     };
-        var [post_content, set_post_content] = useState("")
 
         const PostContent = () => {
             const {post_id} = useParams();
-            const [post_content, set_post_content] = useState("")
+            const [postContent, setPostContent] = useState({
+                data: null,
 
-            getPostByID(post_id).then((response) => {
-                console.log(response.data)
-                //set_post_content(response.data);
             })
-            // set_post_content = getPostByID(post_id);
-            // console.log(set_post_content)
-            console.log("PostContent");
-            // console.log(post_content.data);
+            useEffect(()=>{
+                setPostContent({
+                    data: null,
+                })
+                getPostByID(post_id).then((response)=>{
+                    if (response.status === 401) {
+                        refreshToken().then((response)=>{
+                            if (response.status === 200) {
+                                console.log("got a post")
+                                console.log(response.status)
+                                getPostByID(post_id).then((response)=>{
+                                    if (response.status === 200){
+                                        setPostContent({
+                                            data: response.data,
+                                        })
+                                    }
+                                    console.log("...");
+                                })
+                            }
+                            else {
+                                window.location.reload();
+                                window.location.href = '/login';
+                            }
+                        })
+                    } else if (response.status === 200) {
+                        setPostContent({
+                            data: response.data,
+                        })
+                    } else if (response.status === 404) {
+                        setPostContent({
+                            data: "404"
+                        })
+                    }
+                })
+            }, [])
 
-            return (
-                <p>post_content: {post_content.data}</p>
-            );
-        }
+
+            let content = null;
+            console.log(postContent.data);
+            if (postContent.data) {
+                if (postContent.data === "404") {
+                    content = (<h1 classname="no-content">Post does not Exist</h1>)
+                }
+                else {
+                    if (postContent.data.contentType === "text/markdown") {
+                        console.log("text markdown");
+                    }
+                    else if (postContent.data.contentType === "text/plain") {
+                        console.log("text plain");
+                        content = (
+                            <>
+                                <h2 classname="post-title">{postContent.data.title}</h2>
+                                <p classname="post-content">{postContent.data.content}</p>
+                            </>
+                            )
+                    }
+                    else if (postContent.data.contentType === "application/base64") {
+                        console.log("app/base64");
+                    }
+                    else if (postContent.data.contentType === "image/png;base64") {
+                        console.log("png;base64");
+                        content = (<img classname="no-post-img" src={no_img} alt="pngbase64"></img>)
+                    }
+                    else if (postContent.data.contentType === "image/jpeg;base64") {
+                        console.log("jpeg;base64");
+                        content = (<img classname="no-post-img" src={no_img} alt="jpegbase64"></img>)
+
+                    }
+                    }
+                }
+                return content
+                }
+
+        
+
     
     
 
@@ -54,7 +117,9 @@ export default function PostView() {
 
 
         return (
+            
             <form onSubmit={handleSubmit}>
+                <PostContent/>
                 <label>Comment:
                     <input
                         type="text"
@@ -63,7 +128,7 @@ export default function PostView() {
                     />
                 </label>
                 <button className='comment-submit-button'>Send Comment</button>
-                <PostContent/>
+                
             </form>
             
         )
@@ -80,7 +145,7 @@ export default function PostView() {
                 <h1 className='postview-title'>PostView</h1>
             </div>
             <div className='postview-main'>
-                <img className="no-post-img" src={no_img} alt="No logo" />
+                <postContent/>
                 <CommentForm />
                 
             </div>
