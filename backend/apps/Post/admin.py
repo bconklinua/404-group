00bacb77190cb98dbd04a907179c64f37ff1b6bd
AGLandberg <landberg@ualberta.ca>
2022-10-31 19:27:30 -0600

    Add optional image field to Posts

--- backend/apps/Post/admin.py
+++ backend/apps/Post/admin.py
@@ -4,7 +4,7 @@ from .models import Category, Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'description', 
     'contentType', 'content', 'author', 'get_categories', 'published',
-    'visibility', 'unlisted')
+    'visibility', 'unlisted', 'image')
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
