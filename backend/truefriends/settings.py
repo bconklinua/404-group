42261fc4e48ba53aa41f6617f8414b0d36a7b015
Saurab Harish Prabhakar <saurab@ualberta.ca>
2022-11-14 22:31:58 -0700

    production!

--- backend/truefriends/settings.py
+++ backend/truefriends/settings.py
@@ -103,6 +103,8 @@ WSGI_APPLICATION = 'truefriends.wsgi.application'
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
