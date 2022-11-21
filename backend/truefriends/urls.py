6b41491245be0a811cf1e63d98f22f51c74128b9
Saurab Harish Prabhakar <saurab@ualberta.ca>
2022-11-20 21:03:32 -0700

    revert!

--- backend/truefriends/urls.py
+++ backend/truefriends/urls.py
@@ -87,4 +87,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATICFILES_DIRS)
+#urlpatterns += static(settings.STATICFILES_DIRS)
