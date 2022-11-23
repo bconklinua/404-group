8e02d660583a89f63fb4f8cc1db6adf684988a34
AGLandberg <landberg@ualberta.ca>
2022-11-22 21:27:56 -0700

    Adjust Comment view

--- backend/Comment/views.py
+++ backend/Comment/views.py
@@ -53,15 +53,3 @@ class AuthorCommentView(viewsets.ModelViewSet):
                 else:
                     return Comment.objects.filter(author_id=-1)
         return Comment.objects.filter(author_id=-1)
-
-
-class Assets(View):
-
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
-
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
