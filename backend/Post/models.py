390347bc0ab13a9d7fcb055a92681176f787eeaa
AGLandberg <landberg@ualberta.ca>
2022-11-23 23:18:13 -0700

    Fix issues

--- backend/Post/models.py
+++ backend/Post/models.py
@@ -37,7 +37,7 @@ def upload_to(instance, filename):
 # Create your models here.
 class Post(models.Model):
 
-    id = models.CharField(primary_key=True, default=uuid.uuid4, editable=True, max_length=100)
+    # id = models.CharField(primary_key=True, default=uuid.uuid4, editable=True, max_length=20)
     type = models.CharField(default="post", editable=False, max_length=4)
     title = models.CharField(max_length=30, default="Untitled", unique=True)
     source = models.SlugField(max_length=100, editable=False)
