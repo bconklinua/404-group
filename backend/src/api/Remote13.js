aa2da9fbf71c50ad71eadafc8a0c114c01d09603
Joshua Nam <60495431+JoshuaN18@users.noreply.github.com>
2022-11-25 14:14:05 -0700

    fixed some stuff

--- backend/src/api/Remote13.js
+++ backend/src/api/Remote13.js
@@ -3,10 +3,7 @@ import axios from 'axios'
 export function Team13Users(){
     const url = 'https://cmput404-team13.herokuapp.com/authors?page=1&size=1000000'
     return axios.get(url, {
-        auth: {
-            username: 'team12',
-            password: '96%fmA54'
-        },
+
         headers:{
             "Content-Type": "application/json",
 
